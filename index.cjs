3131c1bb4f2c3e3542dde63be6c5a0c322329d54
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-10-21 08:31:17 +0200

    changed env

--- index.cjs
+++ index.cjs
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require("mongoose");
 const cors = require('cors');
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 const helmet = require("helmet");
 const http = require('http')
 
