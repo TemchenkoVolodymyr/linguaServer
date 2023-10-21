7756f2a26e4f68c9ed01fc8a1eaa774f8c4d657b
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-10-21 08:29:07 +0200

    added PORT for env

--- index.cjs
+++ index.cjs
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require("mongoose");
 const cors = require('cors');
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
 const helmet = require("helmet");
 const http = require('http')
 
