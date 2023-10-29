5ea8c50187d54ad2b2662b48a45e1c3132950add
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-10-29 13:42:37 +0100

    have chaged http

--- index.cjs
+++ index.cjs
@@ -17,8 +17,8 @@ const path = require('path')
 const authUsers = require('./server/Modules/AuthorizationModules.cjs')
 app.use(helmet());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
-app.use(cors({origin: "https://linguaswap.space"}));
-app.options("https://linguaswap.space", cors());
+app.use(cors({origin: "http://linguaswap.space"}));
+app.options("http://linguaswap.space", cors());
 
 
 app.use(express.json())
@@ -27,7 +27,7 @@ const {Server} = require('socket.io')
 
 const io = new Server(server, {
   cors: {
-    origin: "https://linguaswap.space",
+    origin: "http://linguaswap.space",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["lingua-header"],
     credentials: true
